22c822c469a082b19359dd344da07db7cb022f1b
caokejian_i <caokejian_i@didiglobal.com>
2024-01-05 15:07:04 +0800

    test

--- my-pinia/src/components/TodoList.vue
+++ my-pinia/src/components/TodoList.vue
@@ -10,6 +10,6 @@ import { ref } from 'vue'
 import TodoTab from './TodoTab.vue'
 import TodoForm from './TodoForm.vue'
 import Todos from './Todos.vue'
-
+console.log(1)
 </script>
 <style lang='less' scoped></style>
